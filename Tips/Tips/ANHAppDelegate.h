a8b96d3c92ef4e6d3c0a72abc62b7c0de7e8bf10
Anh Nguyen <anh@Anhs-Mac.local>
2013-02-20 17:09:10 -0500

    Added Tips

---
+++ Tips/Tips/ANHAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  ANHAppDelegate.h
+//  Tips
+//
+//  Created by Anh Nguyen on 2/20/13.
+//  Copyright (c) 2013 Anh Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ANHViewController;
+
+@interface ANHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ANHViewController *viewController;
+
+@end
---
+++ Tips/Tips/ANHViewController.h
@@ -0,0 +1,13 @@
+//
+//  ANHViewController.h
+//  Tips
+//
+//  Created by Anh Nguyen on 2/20/13.
+//  Copyright (c) 2013 Anh Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANHViewController : UIViewController
+
+@end
