ba42f2501549aa4a23e31cb94319d8e6f62c36d1
Anh Nguyen <anh@Anhs-Mac.local>
2013-02-20 19:45:18 -0500

    Finished Tips

--- Tips/Tips/ANHViewController.h
+++ Tips/Tips/ANHViewController.h
@@ -10,4 +10,29 @@
 
 @interface ANHViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+
+@property (weak, nonatomic) IBOutlet UISlider *tipSlider;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *peopleSegment;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *percentLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UILabel *personLabel;
+
+
+@property (nonatomic) double billAmount;
+@property (nonatomic) double tipPercent;
+@property (nonatomic) double tipAmount;
+@property (nonatomic) double total;
+@property (nonatomic) double pricePerPerson;
+@property (nonatomic) double people;
+
+- (void) calculate;
+- (void) updateValue;
+- (IBAction)backGroundTap:(id)sender;
+- (IBAction)billChanged:(UITextField *)sender;
+- (IBAction)sliderChanged:(UISlider *)sender;
+- (IBAction)segmentChanged:(UISegmentedControl *)sender;
+
 @end
--- Tips/Tips/ANHViewController.m
+++ Tips/Tips/ANHViewController.m
@@ -18,6 +18,12 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.billAmount = 0;
+    self.tipPercent = 15;
+    self.tipAmount = 0;
+    self.total = 0;
+    self.pricePerPerson = 0;
+    self.people = 1;
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +32,37 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)backGroundTap:(id)sender{
+    [sender resignFirstResponder];
+    [self.billField resignFirstResponder];
+}
+
+- (IBAction)billChanged:(UITextField *)sender {
+    NSString *newBill = sender.text;
+    self.billAmount = [newBill doubleValue];
+    [self calculate];
+}
+
+- (IBAction)sliderChanged:(UISlider *)sender {
+    self.tipPercent= sender.value;
+    self.percentLabel.text = [NSString stringWithFormat:@"%.0f",self.tipPercent];
+    [self calculate];
+}
+
+- (IBAction)segmentChanged:(UISegmentedControl *)sender {
+    self.people = sender.selectedSegmentIndex + 1;
+    [self calculate];
+}
+
+- (void) calculate{
+    self.tipAmount = self.billAmount*self.tipPercent/100;
+    self.total = self.billAmount+self.tipAmount;
+    self.pricePerPerson = self.total/self.people;
+    [self updateValue];
+}
+- (void) updateValue{
+    self.tipLabel.text = [NSString stringWithFormat:@"%.2f",self.tipAmount];
+    self.totalLabel.text = [NSString stringWithFormat:@"%.2f",self.total];
+    self.personLabel.text = [NSString stringWithFormat:@"%.2f",self.pricePerPerson];
+}
 @end
